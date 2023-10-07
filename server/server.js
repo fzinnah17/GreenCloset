b4b4648c7c868b96586ed0802d0d1c3fb58986ff
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-10-07 12:38:05 -0400

    set up Node.js project

---
+++ server/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import path from 'path';
+import favicon from 'serve-favicon';
+import dotenv from 'dotenv';
+// import eventsRoutes from './routes/eventsRoutes.js';
+// import locationsRoutes from './routes/locationsRoutes.js';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+
+// // API routes
+// app.use('/api/events', eventsRoutes);
+// app.use('/api/locations', locationsRoutes);
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(favicon(path.resolve('../', 'client', 'public', 'party.png')));
+} else if (process.env.NODE_ENV === 'production') {
+    app.use(favicon(path.resolve('public', 'party.png')));
+    app.use(express.static(path.resolve('public')));
+}
+
+if (process.env.NODE_ENV === 'production') {
+    app.get('/*', (_, res) =>
+        res.sendFile(path.resolve('..', 'client', 'dist', 'index.html'))
+    );
+}
+
+
+app.listen(PORT, () => {
+    console.log(`server listening on http://localhost:${PORT}`);
+});
