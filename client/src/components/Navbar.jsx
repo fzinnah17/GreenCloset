7b7db1e244ddf44ed5f1bfaa301aefc660ee6701
Farnaz Zinnah <fzinnah000@citymail.cuny.edu>
2023-10-07 13:19:32 -0400

    basic homepage added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "../css/Navbar.css"
+import "../css/Navbar.css";
 
 const Navbar = () => {
     return (
-        <nav className="nav-container bg-light">
+        <nav className="nav-container">
             <Link className="brand" to="/">Climate Hack Columbia University</Link>
         </nav>
     );
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className="homepage-container">
+            Hi the app is working
+        </div>
+    );
+}
+
+export default HomePage;
