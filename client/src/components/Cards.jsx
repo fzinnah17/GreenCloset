dcfde5da8741ffc9f6f0bc7502b1be38faf9cead
mariana <mariana@LAPTOP-H7S8DD55.localdomain>
2023-10-07 22:45:55 -0400

    added color and boxes

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,24 +1,43 @@
-import React from 'react';
+
+import React, { useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import "../css/Cards.css";
 
 const Cards = () => {
-    return (
-       <div>
-         <h1>Recommended</h1> 
-        <div className="container">
-            <div className="item">
-            <p>1</p>
-            </div>
-            <div className="item">
-                <p> 2</p>
+    const [products, setProducts] = useState([]);
+    const navigate = useNavigate();
 
+    useEffect(() => {
+        fetch('/api/productsPage')
+            .then(response => response.json())
+            .then(data => setProducts(data))
+            .catch(error => console.error('There was an error fetching products:', error));
+    }, []);
+    return (
+        <div className="product-container">
+        {products &&
+          products.slice(0, 6).map((product, index) => (
+            <div key={index} className="product-card">
+              <img
+                className="productcard-image"
+                src={product.image}
+                alt={product.name}
+              />
+              <div className={`product-cover product-cover${(index % 3) + 1}`}>
+                <h1 style={{ margin: "0.25em" }}>{product.name}</h1>
+                <h2
+                  className="description-text"
+                  style={{ margin: "0.1em", marginBottom: "1.5em" }}
+                >
+                  {product.description}
+                </h2>
+                <button onClick={() => navigate(`/view/${product.id}`)}>View</button>
+              </div>
             </div>
-         
+          ))}
+      </div>
       
-      
-        </div>
-        </div>
 
         
     );
--- client/src/components/Hook.jsx
+++ client/src/components/Hook.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "../css/Hook.css";
-import hookpic from './pic1person.jpg';
+import hookpic from './boutique.jpg';
 
 
 const Hook = () => {
@@ -11,21 +11,19 @@ const Hook = () => {
     <div className="hook"> 
     
        <h1 className='GC'>Green Closet </h1> 
-       <div classsName='cta-container'>
-       <p className='description'>
-       Clothing the Future, One Wardrobe at a Time:  
-        in GreenCloset you can share your closet and find a treasure, while you help the planet.
+      
+      <body>
+      <div className="page-container">
+      <p className="content">Clothing the Future, One Wardrobe at a Time:  
+        in GreenCloset you can share your closet and find a treasure, while you help the planet.</p>
        
-       </p>
-       <div className="image">
-       <img src={hookpic} alt="pic" width="300" height="300" align="middle"/>
+       <img src={hookpic} alt="pic" width="270" height="270" align="middle" className="right-image"/>
+       
+       
        </div>
-       </div>
-
        
+       </body>
 
-
-       
         </div>
 
         
