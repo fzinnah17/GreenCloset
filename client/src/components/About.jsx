9dbc51bc47ac8f6ed405cb949d634f7c442d1d03
mariana <mariana@LAPTOP-H7S8DD55.localdomain>
2023-10-07 23:27:29 -0400

    fixed borders, added color

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -11,12 +11,14 @@ const About = () => {
        <p className='aboutp'>
        At GreenCloset, we are dedicated to making a meaningful impact in low-income communities, where many individuals struggle to access basic necessities, including clothing. In the context of New York City colleges, where over 50% of students face financial challenges, we've developed a unique resource—an innovative mobile and web app that facilitates the easy exchange of clothing. 
 </p>
+    <div className="box">
         <p className='p2'>
         Our mission is to ensure that individuals always have clothes on their backs, especially for important occasions like first interviews and meetings. Through the power of giving, we are creating a supportive community where everyone can access the attire they need to succeed, bridging the gap for the countless low-income individuals who face clothing insecurity. 
 
         </p>
          <div className="imageAbout">
        <img src={Aboutpic} alt="pic" width="300" height="300" align="middle"/>
+       </div>
        </div>
 
         
